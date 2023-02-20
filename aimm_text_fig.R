ad6b22d85ba8c9a22223528406547b3babe7dea5
wengxyu1030 <xweng@ucdavis.edu>
2023-02-20 00:05:11 -0500

    Create aimm_text_fig.R
    update the sub file for fig.

---
+++ aimm_text_fig.R
@@ -0,0 +1,96 @@
+#' Title: aimm_text_extract.R
+#' Summary:
+#' This script is aimed to scrap text narratives from a nested folder structure containing this narrative across multiple word files. 
+
+#Load necessary files
+library(tidytext)
+library(tidyverse)
+library(tidyr)
+library(purrr)
+library(readxl)
+library(janitor)
+library(xlsx)
+library(readtext)
+library(officer)
+
+
+# Read folder location address from file -----
+folder_paths <- read.csv(file = "./file_location.csv") %>%
+  mutate(path = gsub("\\\\", "/", path)) #' The file has location for both delegated and panel approved projects
+
+if (grepl("xweng", getwd(),ignore.case = TRUE)) {
+  folder_paths <- folder_paths %>%
+    mutate(path = gsub("gjain5","xweng",path))
+} # If "xweng" is using this file, replace path "gjain5" with "xweng"
+
+#' Considering the variation in how different sector store their information, it might be wise to split this into sections.
+#' Currently doing conditions based on sector only but possible to nuance further based on approval type. 
+
+# Extract the narrative -----
+read_paths <- function(fldr_paths){
+  # identify the files and needed docs. (write a function )
+  get_path <- function(fldr_path){list.files(path = fldr_path ,recursive = TRUE) %>%
+      as.data.frame() %>%
+      rename(file_name = '.') %>%
+      mutate(dr = file_name) %>% #use this as directory
+      mutate(file_name = sub(".*/", "", dr)) %>% #get the file name when there are multiple layers of files
+      mutate(fld_name = sapply(dr, function(x) {
+        parts <- strsplit(x, "/")[[1]]
+        part <- parts[grep("\\b[0-9]{5}\\b", parts)][1]
+        part
+      })) %>%
+      mutate(fld_name = case_when( grepl("Amaggi Cotton", file_name) ~ "Amaggi Cotton(43740)",  
+                                   grepl( "Rupshi Foods final document", file_name) ~ "Rupshi Foods final document (46329)",
+                                   #correct cases where the project number is not specified in the folder name.(above are just fixation for delegated_mas files)
+                                   TRUE ~ fld_name)) %>%
+      mutate(id = ifelse(grepl("\\b\\d{5}\\b", fld_name), 
+                         as.numeric(gsub("[^[:digit:]]", "", regmatches(fld_name, regexpr("\\b\\d{5}\\b", fld_name)))), 
+                         NA)) %>%
+      filter(!grepl(".pptx|.xlsx|irm|assistant|is report|is final report|pds|esap|esrs|model",file_name,ignore.case = TRUE)) %>% #Maybe we need IS report later (content), concept note (abstract)
+      filter(grepl("board paper|narrative|aimm",file_name,ignore.case = TRUE)) %>% #filter out AIMM narrative documents.
+      mutate(full_dir = paste0(fldr_path,"/",dr))%>%
+      filter(grepl(".docx$",dr)) #there's pdf potentially can be used.
+  }
+  flds <- lapply(fldr_paths, get_path) %>%
+    do.call(rbind,.)
+  
+  # use the directory to extract the text. 
+  file_list <- flds$full_dir
+  
+  aimm <- purrr::map_dfr(flds$full_dir, function(file) {
+    tryCatch({
+      doc <- officer::read_docx(file)
+      summary <- officer::docx_summary(doc)
+      tibble(summary = summary, folder_path = file)
+    }, error = function(e) {
+      message(sprintf("Error reading file %s: %s", file, e$message))
+      tibble(summary = NA, folder_path = file)
+    })
+  })
+  
+  final <- flds %>%
+    select(id,file_name,full_dir) %>%
+    right_join(aimm,c("full_dir" = "folder_path")) 
+  
+  final
+}
+
+sector_select <- "FIG"
+
+# Define the folder paths
+del_path <- folder_paths %>%
+  filter(approval_type == "manager" & sector == sector_select) %>%
+  pull(path)
+
+pan_path <- folder_paths %>%
+  filter(approval_type == "panel" & sector == sector_select) %>%
+  pull(path)
+
+fldr_paths <- c(del_path,pan_path)
+
+# Extract AIMM narrative
+final <- read_paths(fldr_paths)
+
+# Export file
+save(final, file = paste0(sector_select,".rda"))
+
