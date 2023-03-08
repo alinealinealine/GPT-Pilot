ba855e162a5b3ba7a9ebd0881677844be98bdc38
Gaurav Jain <gauravrpjain@gmail.com>
2023-03-08 13:33:18 -0500

    Updated dataset prep code to align with OpenAI
    Streamlined the prompt, removing instructions. Removed duplicates and checked for token length.

--- src/dataset_prep_finetune.R
+++ src/dataset_prep_finetune.R
@@ -13,19 +13,19 @@ glimpse(AIMM_text)
 
 #Creating Project narrative prompt
 
-AIMM_text$prompt <- ifelse(any(is.na(AIMM_text$text.dev_impct_p),is.na(AIMM_text$text.summary)),NA,
-                                   paste0("Given the following summary for development impact, please write a note on the project outcomes under the IFC Anticipated Impact Measurement and Monitoring (AIMM) framework.\n",
-                                                     str_squish(AIMM_text$text.summary),"Please provide an assessment of the project outcomes in paragraphs.\n###\n"
-                                                     ))
-AIMM_text$completion <- ifelse(any(is.na(AIMM_text$text.dev_impct_p),is.na(AIMM_text$text.summary)),NA,
-                               paste0(str_squish(AIMM_text$text.dev_impct_p))
-                               )
+AIMM_text$prompt <- paste0(str_squish(AIMM_text$text.summary),"\n\n###\n\n"
+                                                     )
+AIMM_text$completion <- paste0(" ",str_squish(AIMM_text$text.dev_impct_p),"\n[END]")
+
+temp <- AIMM_text[,c("prompt","completion")]
+
+temp <- na.omit(temp[!duplicated(temp),])
 
 #Checking for token
 #Checking that no data is more than token limit
-#if(AIMM_text %>% mutate(id=row_number()) %>% tidytext::unnest_tokens(prompt,output = "token") %>% count(id) %>% summarise(highest = max(n)) > 2000){warning("Token limit exceeded")}
+if(temp %>% mutate(id=row_number()) %>% tidytext::unnest_tokens(prompt,output = "token") %>% count(id) %>% summarise(highest = max(n)) > 2000){warning("Token limit exceeded")}
 #if(AIMM_text %>% mutate(id=row_number()) %>% tidytext::unnest_tokens(completion,output = "token") %>% count(id) %>% summarise(highest = max(n)) > 2000){warning("Token limit exceeded")}
 
-jsonlite::stream_out(x = AIMM_text[,c("prompt","completion")],file("./output/FIG-BP-PRO-0001.json"))
+jsonlite::stream_out(x = temp[,c("prompt","completion")],file("./output/FIG-BP-PRO-0001.json"))
 
 gc()
